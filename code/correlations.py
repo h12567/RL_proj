1ef2224f69b28199c643339152d473ddaed32447
chrisboo <christopherboo66@gmail.com>
2018-11-07 13:04:11 +0800

    update correlations

--- code/correlations.py
+++ code/correlations.py
@@ -1,5 +1,4 @@
 import os
-import io
 import numpy as np
 import pandas as pd
 import itertools
@@ -9,34 +8,43 @@ stock_dir = "../data/stock/"
 filenames = []
 for file in os.listdir(stock_dir):
     filename = os.fsdecode(file)
-    filenames.append(filename)
+    filenames.append(filename[:-4])  # remove .csv
 
 n = len(filenames)
+r = 6
 
 list.sort(filenames)
 
 stock_prices = []
 for filename in filenames:
-    stock_csv = pd.read_csv(stock_dir + filename)
+    stock_csv = pd.read_csv(stock_dir + filename + ".csv")
     stock_price = list(stock_csv.Open)
     stock_prices.append(stock_price)
 
 cov_mat = np.cov(stock_prices)
-inv_cov_mat = np.linalg.inv(cov_mat)
+
 
 i = 0
-r = 6
-with io.open('../data/correlation/mvp' + str(r) + '.txt', 'w') as file:
-    for combination in itertools.combinations(range(len(filenames)), r):
-        stocks = list(combination)
+min_variance = 100000000
+best_result = []
+for combination in itertools.combinations(range(len(filenames)), r):
+    stocks = list(combination)
 
-        cov_sub = cov_mat[np.ix_(stocks, stocks)]
-        inv_cov = np.linalg.inv(cov_sub)
-        variance = 1 / inv_cov.sum()
+    cov_sub = cov_mat[np.ix_(stocks, stocks)]
+    inv_cov = np.linalg.inv(cov_sub)
 
-        result = ''.join(filenames[i][:-4] + "," for i in stocks) + str(variance) + "\n"
-        file.write(result)
+    mat_sum = inv_cov.sum()
+    weight = inv_cov.sum(axis=1) / mat_sum
+    variance = 1 / mat_sum
 
-        i += 1
-        if i % 10000 == 0:
-            print(i)
+    result = str(np.array(filenames)[np.ix_(stocks)]) + "," + str(weight) + ',' + str(variance) + "\n"
+
+    if min_variance > variance and (weight > 0).all():  # short selling not allowed
+        min_variance = variance
+        best_result = result
+
+    i += 1
+    if i % 20000 == 0:
+        print(best_result)
+
+print(best_result)
