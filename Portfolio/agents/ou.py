bf7c0741275c26cf82e83fbba2ba34795540380a
chrisboo <christopherboo66@gmail.com>
2018-11-15 09:58:14 +0800

    rewrite Ornstein-Uhlenbeck Algorithm to match desired format

--- Portfolio/agents/ou.py
+++ Portfolio/agents/ou.py
@@ -13,16 +13,18 @@ class OUAgent:
         self.sigma = sigma
         self.dt = dt
         self.x0 = x0
-        self.reset()
 
-    def __call__(self):
-        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
-            self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
-        self.x_prev = x
+    def predict(self, s, a):
+        s = s[0]
+        a = a[0]
+
+        x = get_close_price(s)
+        x += self.theta * (self.mu - x) * self.dt + self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
         return x
-
-    def reset(self):
-        self.x_prev = self.x0 if self.x0 is not None else np.zeros_like(self.mu)
 
     def __repr__(self):
         return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self.mu, self.sigma)
+
+    @staticmethod
+    def get_close_price(s):
+        return [prices[-1][0] for i, prices in enumerate(s)]
